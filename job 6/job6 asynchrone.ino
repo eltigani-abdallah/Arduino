93ce79fdae89c2419f30868526168e9819606943
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-04-08 09:32:47 +0200

    feat: job 6

---
+++ job 6/job6 asynchrone.ino
@@ -0,0 +1,17 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  delay(500); // Wait for 500 millisecond(s)
+}
