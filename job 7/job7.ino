8da0299ca428761690c0ee486ac6070836703e29
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-04-08 09:44:56 +0200

    job 7

---
+++ job 7/job7.ino
@@ -0,0 +1,49 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(12, HIGH);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(11, HIGH);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(10, HIGH);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(7, HIGH);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(6, HIGH);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(5, HIGH);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(4, HIGH);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(12, LOW);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(11, LOW);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(10, LOW);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(7, LOW);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(6, LOW);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(5, LOW);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(4, LOW);
+  delay(500); // Wait for 500 millisecond(s)
+}
